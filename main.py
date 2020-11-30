b2a651299648715995c0725107135d42d0987441
Eduardo Reta <01ereta.n@gmail.com>
2020-11-30 13:44:31 -0600

    Nuevos files

---
+++ main.py
@@ -0,0 +1,20 @@
+import a
+import argparse
+
+def main():
+    # Creamos el objeto ArgumentParser
+    parser = argparse.ArgumentParser() 
+
+    # Creamos los flags para los argumentos
+    parser.add_argument("-path", dest="path", help="")
+    parser.add_argument("-hsh", dest="hashAlg")
+
+    params = parser.parse_args()
+    filePath = params.path
+    hashAlgorithm = params.hashAlg
+    
+    a.hash(filePath, hashAlgorithm)
+           
+if __name__ == '__main__':
+    main = main()    
+    
